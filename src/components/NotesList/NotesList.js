b9e6a0c7a7a71258bd471b877c99527c555a42c9
BhanuSravya-Nissankararao <121043749+BhanuSravya-Nissankararao@users.noreply.github.com>
2023-03-31 13:10:44 +0530

    changes

--- src/components/NotesList/NotesList.js
+++ src/components/NotesList/NotesList.js
@@ -13,3 +13,20 @@ const NotesList = ({ notes, onDelete }) => {
 };
 
 export default NotesList;
+// import React from 'react';
+// import Note from '../Note/Note';
+// import './NotesList.css'
+
+// const NotesList = ({ notes, onDelete }) => {
+//   const notesArray = Array.from({ length: notes }, (_, i) => i + 1); // Convert the number to an array
+
+//   return (
+//     <div className="noteList">
+//       {notesArray.map(note => (
+//         <Note key={note} note={note} onDelete={onDelete} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default NotesList;
